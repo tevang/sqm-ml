56679f6da82c0234acfed064e3dbc86aa3479251
Thomas Evangelidis <tevang@aiffinity.com>
2022-09-26 11:58:56 +0200

    The n_components for UMAP are not controlled from the command line

--- EXEC_master_script.py
+++ EXEC_master_script.py
@@ -35,6 +35,9 @@ DESCRIPTION:
     parser.add_argument("-exec_dir", dest="EXECUTION_DIR_NAME", required=False, type=str, default=None,
                         help="Name of the execution directory where all output files will be saved. "
                              "Default: %(default)s")
+    parser.add_argument("-n_comp", dest="N_COMPONENTS", required=False, type=int, default=None,
+                        help="Number of components for UMAP (if activated). "
+                             "Default: %(default)s")
     parser.add_argument("-f", dest="FORCE_COMPUTATION", required=False, default=False, action='store_true',
                         help="Force computation of scores even if the respective CSV files exist. Default: %(default)s")
 
@@ -42,13 +45,14 @@ DESCRIPTION:
     return args
 
 def launch_pipeline(CROSSVAL_PROTEINS_STRING, XTEST_PROTEINS_STRING, EXECUTION_DIR_NAME, FORCE_COMPUTATION,
-                    Settings=None):
+                    N_COMPONENTS, Settings=None):
 
     if not Settings:
         Settings = settings()
 
     Settings.HYPER_FORCE_COMPUTATION = FORCE_COMPUTATION
     Settings.HYPER_EXECUTION_DIR_NAME = Settings.HYPER_EXECUTION_DIR_NAME if EXECUTION_DIR_NAME is None else EXECUTION_DIR_NAME
+    Settings.N_COMPONENTS = Settings.N_COMPONENTS if N_COMPONENTS is None else N_COMPONENTS
     CROSSVAL_PROTEINS = [p for p in CROSSVAL_PROTEINS_STRING.split(",") if len(p) > 0]
     XTEST_PROTEINS = [p for p in XTEST_PROTEINS_STRING.split(",") if len(p) > 0]
 
@@ -128,4 +132,5 @@ def launch_pipeline(CROSSVAL_PROTEINS_STRING, XTEST_PROTEINS_STRING, EXECUTION_D
 if __name__ == "__main__":
 
     args = cmdlineparse()
-    launch_pipeline(args.CROSSVAL_PROTEINS, args.XTEST_PROTEINS, args.EXECUTION_DIR_NAME, args.FORCE_COMPUTATION)
+    launch_pipeline(args.CROSSVAL_PROTEINS, args.XTEST_PROTEINS, args.EXECUTION_DIR_NAME,
+                    args.FORCE_COMPUTATION, args.N_COMPONENTS)
